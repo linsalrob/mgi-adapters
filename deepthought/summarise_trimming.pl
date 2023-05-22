6ae2affc4cc13cfba2e961389eeb35f81196bc01
Rob Edwards <raedwards@gmail.com>
2023-05-22 17:58:23 +0930

    a script to summarise the numbers of sequences trimmed

--- deepthought/summarise_trimming.pl
+++ deepthought/summarise_trimming.pl
@@ -23,14 +23,17 @@ my ($r1tot, $r2tot, $r1trim, $r2trim, $seqs) = (0, 0, 0, 0, 0);
 
 opendir(DIR, $dir) || die "can't open $dir";
 foreach my $file (grep {$_ !~ /^\./} readdir(DIR)) {
-	if (m/Total sequences: R1 (\d+) R2 (\d+)/) {
-		$r1tot+=$1;
-		$r2tot+=$2;
-		$seqs++;
-	}
-	if (m/Sequences trimmed: R1 (\d+) R2 (\d+)/) {
-		$r1trim+=$1;
-		$r2trim+=$2;
+	open(IN, "$dir/$file") || die "Can't open $dir/$file";
+	while (<IN>) {
+		if (m/Total sequences: R1 (\d+) R2 (\d+)/) {
+			$r1tot+=$1;
+			$r2tot+=$2;
+			$seqs++;
+		}
+		if (m/Sequences trimmed: R1 (\d+) R2 (\d+)/) {
+			$r1trim+=$1;
+			$r2trim+=$2;
+		}
 	}
 }
 print "Directory\tSequence files\tR1 total\tR2 total\tR1 trimmed\tR2 trimmed\n";
