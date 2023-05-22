f631e4b04397414c114fce5d9cd42b5c4bf2aea6
Rob Edwards <raedwards@gmail.com>
2023-05-22 18:05:07 +0930

    a script to summarise the numbers of sequences trimmed

--- deepthought/summarise_trimming.pl
+++ deepthought/summarise_trimming.pl
@@ -9,33 +9,35 @@ This is a simple script that reads the trimming_slurm directory and then summari
 =cut
 
 
-my $dir = shift;
-if (!$dir && -e "trimming_slurm") {
+
+
+my @dir = @ARGV;
+if (!@dir && -e "trimming_slurm") {
 	print STDERR "Using the directory trimming slurm\n";
-	$dir = "trimming_slurm";
+	push @dir, "trimming_slurm";
 }
-if (!$dir) {
+if (!@dir) {
 	die "You didn't provide a directory and we can't find the default trimming_slurm directory\n";
 }
 
 my ($r1tot, $r2tot, $r1trim, $r2trim, $seqs) = (0, 0, 0, 0, 0);
 
-
-opendir(DIR, $dir) || die "can't open $dir";
-foreach my $file (grep {$_ !~ /^\./} readdir(DIR)) {
-	open(IN, "$dir/$file") || die "Can't open $dir/$file";
-	while (<IN>) {
-		if (m/Total sequences: R1 (\d+) R2 (\d+)/) {
-			$r1tot+=$1;
-			$r2tot+=$2;
-			$seqs++;
-		}
-		if (m/Sequences trimmed: R1 (\d+) R2 (\d+)/) {
-			$r1trim+=$1;
-			$r2trim+=$2;
+foreach my $dir (@dir) {
+	print "Directory\tSequence files\tR1 total\tR2 total\tR1 trimmed\tR2 trimmed\n";
+	opendir(DIR, $dir) || die "can't open $dir";
+	foreach my $file (grep {$_ !~ /^\./} readdir(DIR)) {
+		open(IN, "$dir/$file") || die "Can't open $dir/$file";
+		while (<IN>) {
+			if (m/Total sequences: R1 (\d+) R2 (\d+)/) {
+				$r1tot+=$1;
+				$r2tot+=$2;
+				$seqs++;
+			}
+			if (m/Sequences trimmed: R1 (\d+) R2 (\d+)/) {
+				$r1trim+=$1;
+				$r2trim+=$2;
+			}
 		}
 	}
+	print join("\t", $dir, $rob->commify($seqs), $rob->commify($r1tot), $rob->commify($r2tot), $rob->commify($r1trim), $rob->commify($r2trim)), "\n";
 }
-print "Directory\tSequence files\tR1 total\tR2 total\tR1 trimmed\tR2 trimmed\n";
-print join("\t", $dir, $rob->commify($seqs), $rob->commify($r1tot), $rob->commify($r2tot), $rob->commify($r1trim), $rob->commify($r2trim)), "\n";
-
